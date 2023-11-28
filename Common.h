3fca570be4c577d45e442547673ac6ac959b948a
天宇 胡 <2524183995@qq.com>
2023-11-28 21:19:22 +0800

    添加项目文件。

---
+++ Common.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Windows.h>
+#include <wrl.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <windowsx.h>
+#include "..\Common\d3dx12.h"
+#include <comdef.h>
+
+using namespace Microsoft::WRL;
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
