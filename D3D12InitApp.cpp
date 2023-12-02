9fba4f3fbdbec91176c638d9d65556b20fbbd94e
天宇 胡 <2524183995@qq.com>
2023-12-02 13:51:46 +0800

    后面剔除，线框模式

--- D3D12InitApp.cpp
+++ D3D12InitApp.cpp
@@ -370,7 +370,7 @@ void D3D12InitApp::BuildPSO()
         mpsByteCode->GetBufferSize()
     };
     psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-    //psoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
+    psoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
     psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;
     psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
     psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
